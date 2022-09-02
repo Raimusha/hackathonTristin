80d5eb31e726638820b9652f1a994924d944e296
Raimusha <tjarvis5@uncc.edu>
2022-09-02 18:52:13 -0400

    updated changes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("this is a test. hello world");
+import pokebase as pb;
+
+print('Using POKEAPI,this program will return the height and weight of a pokemon');
+
+pokeSearch = input("Please enter Pokemon's name: ");
+pokeData = pb.APIResource('pokemon',pokeSearch);
+
+print(pokeSearch,"'s weight is: ", pb.pokemon(pokeSearch).weight);
+print(pokeSearch,"'s height is: ", pb.pokemon(pokeSearch).height);
+
+print('thank you');
---
+++ readme.md
@@ -0,0 +1,5 @@
+ Hi am Tristin. I am a junior developer.
+
+ This program will get the heigh and weight of pokemon from the pokeAPI.
+
+ Dependencies: Pokebase
